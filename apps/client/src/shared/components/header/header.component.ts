88710ad2499915964a66e19cbf1794a819b19b36
Robin <dev@pulsarr.fr>
2024-10-09 19:45:22 +0200

    chore: theming tailwind and colors

--- apps/client/src/shared/components/header/header.component.ts
+++ apps/client/src/shared/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButton } from "@angular/material/button";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButton],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
