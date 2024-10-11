b201807fcd0254567be08762442bc0a08ccf864c
Robin <dev@pulsarr.fr>
2024-10-11 18:52:31 +0200

    chore: no more angular-material. We will stay with simple tailwind for now

--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -9,6 +9,16 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        background: '#EAEAEA',
+        text: '#121212',
+        secondaryText: '#222222',
+        border: '#222222',
+        accent: '#2563EB',
+      },
+      borderColor: {
+        DEFAULT: '#222222',  // Couleur par défaut pour toutes les bordures
+      },
       spacing: {
         'sidebar': '250px',  // Utilisation pour la largeur de la sidebar par exemple
       },
